70eb9d651d225affe50f79fe8c0f101b9cc3073e
Fedichina <fedichinag@gmail.com>
2023-02-28 06:24:40 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different
+ * combinations of two digits
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	for (b = 0; b <= 9; b++)
+	{
+		putchar('0' + a);
+		putchar('0' + b);
+
+		if (a != 9 || b != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
