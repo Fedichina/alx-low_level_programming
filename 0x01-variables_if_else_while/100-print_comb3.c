566967f6c32d40310d980e6355b7175ad5943d8f
Fedichina <fedichinag@gmail.com>
2023-02-28 07:49:47 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,20 +7,22 @@
  */
 int main(void)
 {
-	int a;
-	int b;
+	int a, b;
 
 	for (a = 0; a <= 8; a++)
 	for (b = 1; b <= 9; b++)
 	{
+	if (a != b)
+	{
 		putchar('0' + a);
 		putchar('0' + b);
 
-		if (a != 9 || b != 9)
+		if (a != '8' || b != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	}
 	putchar('\n');
 	return (0);
