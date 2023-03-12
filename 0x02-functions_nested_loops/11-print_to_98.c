6e6329331f622ee7b35eb5a50d532b1af1f8b493
Fedichina <fedichinag@gmail.com>
2023-03-12 00:12:34 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,21 +7,13 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	for (n = 0; n < 99; n++)
 	{
-		while (n > 98)
-		{
-			printf("%d,", n--);
-			printf("%d\n", n);
-		}
+	printf("%d,", n);
 	}
-
-		else
-		{
-		while (n < 98)
-		{
-			printf("%d,", n++);
-			printf("%d\n", n);
-		}
-		}
+	for (n = 0; n > 98; n--)
+	{
+	printf("%d,", n);
+	}
+printf("\n");
 }
